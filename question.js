7ec425b1818e6bab3096534f1099538da64a0268
raheemii-hub <62184307+raheemii-hub@users.noreply.github.com>
2020-05-10 12:21:54 +0100

    Create question.js

---
+++ question.js
@@ -0,0 +1,36 @@
+var questions = [{
+    "question": "who invented calculator?",
+    "option1": "Charle Babbage",
+    "option2": "Blaise Pascal",
+    "option3": "isaac Newton",
+    "option4":"Thomas Edision ",
+    "answer": "2"
+},{
+    "question": "How many generations of computer do we have?",
+    "option1": "5",
+    "option2": "1",
+    "option3":"3",
+    "option4":"4",
+    "answer": "1"
+},{
+    "question": "What is the full meaning of WWW?",
+    "option1":"Wide Web Wide",
+    "option2":"Wide Web World",
+    "option3":"World Wide Web",
+    "option4":"Web Web Web",
+    "answer":"3"
+},{
+    "question":"How many buttons does a mouse has?",
+    "option1":"2",
+    "option2":"5",
+    "option3":"3",
+    "option4":"1",
+    "answer": "1"
+},{
+    "question":"Which of the follow is an input device?",
+    "option1":"Mouse",
+    "option2":"Speaker",
+    "option3":"Monitor",
+    "option4":"Printer",
+    "answer":"1"
+}]
